aa144a4d4dd99cf4470eb4787939dafbdcb9225b
MarvinKlein1508 <support@marvinkleinmusic.de>
2023-11-08 11:08:18 +0100

    Add JsonTypeHandler

---
+++ DbController.TypeHandler/JsonTypeHandler.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using System.Data;
+using System.Text.Json;
+
+namespace DbController.TypeHandler
+{
+    /// <summary>
+    /// TypeHandler to serialize and deserialize JSON objects
+    /// </summary>
+    public class JsonTypeHandler : SqlMapper.ITypeHandler
+    {
+        public void SetValue(IDbDataParameter parameter, object value)
+        {
+            if (value is null)
+            {
+                parameter.Value = null;
+            }
+            else
+            {
+                parameter.Value = JsonSerializer.Serialize(value);
+            }
+        }
+
+        public object Parse(Type destinationType, object value)
+        {
+            if (value is null)
+            {
+                return null;
+            }
+            else
+            {
+                return JsonSerializer.Deserialize(value as string, destinationType);
+            }
+        }
+    }
+}
