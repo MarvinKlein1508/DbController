15e4ea83d9f24102a2ff9382c82ee2991c752338
MarvinKlein1508 <support@marvinkleinmusic.de>
2024-04-25 13:17:22 +0200

    Add IDbModel<TIdentifier> & deprecate older models

--- DbController/IDbModel.cs
+++ DbController/IDbModel.cs
@@ -1,8 +1,21 @@
 ﻿namespace DbController
 {
     /// <summary>
+    /// Defines a database model.
+    /// </summary>
+    public interface IDbModel<TIdentifier> : IDbParameterizable
+    {
+        /// <summary>
+        /// Gets the unique database identifier for the object.
+        /// </summary>
+        TIdentifier GetIdentifier();
+    }
+
+
+    /// <summary>
     /// Defines a databse model which can be indexed by an unique <see cref="Id"/>
     /// </summary>
+    [Obsolete("This interface is deprecated and should no longer be used. Use IDbModel<TIdentifier> instead.")]
     public interface IDbModel : IDbParameterizable
     {
         /// <summary>
@@ -18,6 +31,7 @@
     /// This interface should only be used when you don't plan on using localization for your database objects.
     /// </para>
     /// </summary>
+    [Obsolete("This interface is deprecated and should no longer be used. Use IDbModel<TIdentifier> instead.")]
     public interface IDbModelWithName : IDbModel
     {
         /// <summary>
@@ -29,6 +43,7 @@
     /// <summary>
     /// Defines a database model which supports one or more localizable properties.
     /// </summary>
+    [Obsolete("This interface is deprecated and should no longer be used. Use IDbModel<TIdentifier> instead.")]
     public interface ILocalizedDbModel : IDbModel
     {
         /// <summary>
